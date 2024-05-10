771760f2390b37571eca5f3c9e592aa802c68d0a
Sumit Man <summitmman@gmail.com>
2024-05-10 23:43:48 +0530

    Updates

---
+++ assets/WidgetsRenderer-DBO2oZ9d.js
@@ -0,0 +1,1 @@
+import{b as m}from"./PageCrafter.vue_vue_type_script_setup_true_lang-DAWBmiHY.js";import"./index-DUXUg2Vn.js";export{m as default};
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import PageCrafter from './pageCrafter/PageCrafter.vue';
-
-export type { IPage, GenericObject, EventMap } from './pageCrafter/shared/interfaces';
-export default {
-    install: (app: any) => {
-        app.component("PageCrafter", PageCrafter);
-    }
-};
