99d071dda27e0cbbd35f192d102a5458af54114f
Sumit Man <summitmman@gmail.com>
2024-06-15 05:15:11 +0530

    Updates

--- assets/VIf-YyIzInI6.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/WidgetsRenderer-B7XvsIra.js","assets/PageCrafter.vue_vue_type_script_setup_true_lang-WSpottVt.js","assets/index-D2_2riKq.js","assets/index-Bi55jKNN.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{d as i,aR as r,o,a as d,a1 as p,f as c,i as l,_ as s}from"./index-D2_2riKq.js";const v=i({__name:"VIf",props:{condition:{type:[String,Number,Boolean,Object,Function],required:!0},vElseChildren:{type:Array,default:()=>[]},widgetMap:{type:Object,default:()=>{}},eventMap:{type:Object,default:()=>{}},reactiveVariableMap:{type:Object,default:()=>{}}},setup(t){const a=l(()=>s(()=>import("./WidgetsRenderer-B7XvsIra.js"),__vite__mapDeps([0,1,2,3]))),e=t;return(n,u)=>(typeof e.condition=="function"?e.condition():e.condition)?r(n.$slots,"default",{key:0}):e.vElseChildren.length?(o(),d(p(a),{key:1,widgets:e.vElseChildren,widgetMap:e.widgetMap,eventMap:e.eventMap,reactiveVariableMap:e.reactiveVariableMap},null,8,["widgets","widgetMap","eventMap","reactiveVariableMap"])):c("",!0)}});export{v as default};
--- assets/WidgetsRenderer-B7XvsIra.js
+++
@@ -1,1 +0,0 @@
-import{b as m}from"./PageCrafter.vue_vue_type_script_setup_true_lang-WSpottVt.js";import"./index-D2_2riKq.js";export{m as default};
